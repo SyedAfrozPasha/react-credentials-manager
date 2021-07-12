4a9d32739818b042edbbc66df2e9e22720b27dc6
SyedAfrozPasha <afroz.pasha26@gmail.com>
2021-07-12 15:05:28 +0530

    1 file modified

--- src/Components/CredsManager.js
+++ src/Components/CredsManager.js
@@ -64,16 +64,16 @@ export default function CredsManager() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
               className="mr-2"
             >
               <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-              <polyline points="7 10 12 15 17 10" />
-              <line x1="12" y1="15" x2="12" y2="3" />
+              <polyline points="17 8 12 3 7 8" />
+              <line x1="12" y1="3" x2="12" y2="15" />
             </svg>
-            <span>Export</span>
+            <span>Import</span>
           </button>
           <div className="dropdown hover:block inline-block relative ml-2">
             <button className="bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center">
