204d1d3c15495e602a68a8c75e53aaed50f75afb
SyedAfrozPasha <afroz.pasha26@gmail.com>
2021-07-20 18:26:04 +0530

    1 file modified

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -35,7 +35,9 @@ export default function Modal({
           >
             <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
           </svg>
-          <span className="text-base text-gray-700 font-medium">{title}</span>
+          <span className="text-base text-gray-700 font-medium ml-1">
+            {title}
+          </span>
         </div>
         <div className="bg-grey-lighter px-4 py-2">
           <div>{message}</div>
