d2247253513ae06d15590997e444f5ce9dbfd0f1
SyedAfrozPasha <afroz.pasha26@gmail.com>
2021-07-20 12:09:03 +0530

    1 file modified

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,13 @@
 import React, { useContext, useState } from 'react';
 import { Tooltip } from 'react-tippy';
-import Modal from 'react-modal';
+import Modal from './Modal';
 // import { AuthContext, CardContext } from '../App';
 
 function Header({ authDispatch, cardDispatch }) {
   // const authContext = useContext(AuthContext);
   // const cardContext = useContext(CardContext);
 
-  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [modalIsOpen, setModalIsOpen] = React.useState(false);
 
   const handleLogout = () => {
     authDispatch({
@@ -22,21 +22,13 @@ function Header({ authDispatch, cardDispatch }) {
     localStorage.setItem('isLoggedIn', false);
   };
 
-  const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-      padding: 0,
-      border: 0,
-      boxShadow: '0 25px 50px -12px rgba(0, 0, 0, .25)'
-    }
+  const modelOpen = () => {
+    setModalIsOpen(true);
   };
 
-  console.log('HEADER');
+  const modelClose = () => {
+    setModalIsOpen(false);
+  };
 
   return (
     <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-4">
@@ -58,7 +50,7 @@ function Header({ authDispatch, cardDispatch }) {
         <Tooltip title="Logout">
           <button
             className="outline-none focus:outline-none"
-            onClick={handleLogout}
+            onClick={modelOpen}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -78,32 +70,15 @@ function Header({ authDispatch, cardDispatch }) {
           </button>
         </Tooltip>
       </div>
-      <Modal isOpen={modalIsOpen} style={customStyles}>
-        <div className="box border border-teal-500 rounded flex flex-col bg-white">
-          <div className="flex justify-start bg-grey-lighter px-4 py-2 border-b">
-            <svg
-              className="fill-current h-4 w-4 text-red-500 mr-1"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              style={{ marginTop: '0.3rem' }}
-            >
-              <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
-            </svg>
-            <span className="text-base text-gray-700 font-medium">Delete</span>
-          </div>
-          <div className="bg-grey-lighter px-4 py-2">
-            <div>Are you sure you want to delete?</div>
-          </div>
-          <div className="flex justify-end bg-grey-lighter px-4 py-2">
-            <button class="bg-transparent hover:bg-teal-500 text-teal-700 font-semibold hover:text-white py-1 px-4 border border-teal-500 hover:border-transparent rounded">
-              Cancel
-            </button>
-            <button class="bg-red-600 hover:bg-red-700 text-white font-semibold py-1 px-4 ml-2 rounded">
-              Delete
-            </button>
-          </div>
-        </div>
-      </Modal>
+
+      <Modal
+        modalIsOpen={modalIsOpen}
+        title="Logout"
+        message="Are you sure you want to logout?"
+        buttonTitle="Logout"
+        onModalClose={modelClose}
+        onModalButtonAction={handleLogout}
+      />
     </nav>
   );
 }
